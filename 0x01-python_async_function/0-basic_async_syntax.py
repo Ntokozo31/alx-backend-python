b499422c780b58b7643ecd8c484d3ac0444478aa
Ntokozo31 <dubentokozo45@yahoo.com>
2024-10-14 19:34:26 +0000

    Added typed-annoted in all the functions

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -14,6 +14,6 @@ async def wait_random(max_delay: int = 10) -> float:
     Return:
         float: The acual time waited, as float.
     """
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
