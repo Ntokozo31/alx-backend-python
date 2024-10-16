d44fbbd8d035774fccecb462d7aaf5428dfdcc9f
Ntokozo31 <dubentokozo45@yahoo.com>
2024-10-16 19:56:12 +0000

    async-generate a random number

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+This module proveide the
+Async Generator
+"""
+
+import asyncio
+from typing import AsyncGenerator
+import random
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Coroutin will loop 10 times each time async wait 1 second
+    then yield a random number from 0 to 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,1 @@
+THIS IS THE README FILE FOR 0x02-python_async_comprehension
