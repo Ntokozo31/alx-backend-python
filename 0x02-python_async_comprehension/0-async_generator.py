8f67a3b18d54c3e952ea8983fbd5c46dbb2e99f1
Ntokozo31 <dubentokozo45@yahoo.com>
2024-10-18 18:32:02 +0000

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
-"""
-This module proveide the
-Async Generator
-"""
+"This module proveide the function that generate random number"
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Coroutin will loop 10 times each time async wait 1 second
     then yield a random number from 0 to 10
