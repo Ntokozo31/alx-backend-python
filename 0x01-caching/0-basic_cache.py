1048d0ffdbb3f7073056601e25c9e149fbd32501
Ntokozo31 <dubentokozo45@yahoo.com>
2024-10-30 17:28:14 +0000

    fixed the documentation

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 class BaseCaching:
     """BaseCaching defines a caching system"""
     def __init__(self):
@@ -8,18 +9,15 @@ class BaseCaching:
 
 class BasicCache(BaseCaching):
     """BasicCache class that inherits from BaseCaching"""
-
     def put(self, key, item):
-         """Assign items into a dictionary self.cache_data with key"""
-         if key is not None and item is not None:
-             self.cache_data[key] = item
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
         """Return the value linked to key in self_cache"""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
-
 
     def print_cache(self):
         """Print the current cache data"""
