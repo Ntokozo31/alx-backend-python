b0aa4f24aa553f175ba23f73e25c0e412fe11737
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-25 15:22:10 +0000

    Complex types - functions

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -10,7 +10,6 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Function that multiplies a float by multiplier
     """
-    
     def multiplier_function(value: float) -> float:
         """
         Function that multies value by multiplier
