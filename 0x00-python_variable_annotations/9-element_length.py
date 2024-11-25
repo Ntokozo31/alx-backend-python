96e9230e86228b1055e110be750a7cea8d638355
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-25 15:48:59 +0000

    Complex types - functions

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,7 +6,7 @@ This module produce type annotation
 from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Function returns list as tuples
     """
