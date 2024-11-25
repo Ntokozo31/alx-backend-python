6e47b532d4511daea6e386fc9e45a75a9a5d403e
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-25 06:25:05 +0000

    concatinated two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+This module provide annotation
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function concatinate two strings
+    """
+    return str1 + str2
