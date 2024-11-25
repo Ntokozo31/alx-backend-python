29a05de7ddd4af2b0aea42faee388c546f73857d
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-25 06:17:07 +0000

    Basic typed annotation add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+"""
+This module produce typing annotation
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    This function adds two float and returns float
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+THIS IS A README FILE FOR 0x00-python_variable_annotations
